7c56c5fa0e31b57409fcc66c27dc8f78e0652f83
Jacob Webber <jacobjwebber@gmail.com>
2016-10-19 17:45:46 +0100

    run script for coursework

--- threaded-programming/coursework1/run.sh
+++ threaded-programming/coursework1/run.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
+reps=6
+rm temp
+rm temploop1
+rm temploop2
+
+do_loops() {
+    echo "SCHEDULE=${OMP_SCHEDULE}"
+
+    for ((i=1;i<=reps+1;i++)); do
+        ./loopspar_runtime >> temp
+        printf "."
+    done
+    
+    cat temp | sed -n '6~4p' > temploop1
+    cat temp | sed -n '8~4p' > temploop2
+ 
+    echo "loop 1 times"
+    cat temploop1
+
+    echo "loop 2 times"
+    cat temploop2
+
+    rm temp temploop1 temploop2
+}
+
+export OMP_NUM_THREADS=4
 export OMP_SCHEDULE="static"
 
-echo "SCHEDULE=STATIC"
+do_loops
 
-for i in {1..10}
-do
-    ./loopspar_runtime >> temp
+export OMP_SCHEDULE="auto"
 
-    cat temp | sed -n '2~4p' > temploop1
-    cat temp | sed -n '4~4p' > temploop2
-done
-
-echo "loop 1 times"
-cat temploop1
-
-echo "loop 2 times"
-cat temploop2
-
-
+do_loops
