e537f4908ac16e00e13377aa90134af98c37eee9
Jacob Webber <s1116548@cplab007.ph.ed.ac.uk>
2016-11-13 13:02:07 +0000

    ready to run performance experiment

---
+++ programming-skills/programming-skills-group03-master/performance/script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+rm total_time.txt
+generate_images() {
+    for i in $(seq -f "%03g" 10 10 400)
+    do
+        convert -compress none *.pnm -resize ${i}% scaled/image_scaled${i}
+    done
+
+    for i in $(ls scaled)
+    do
+        ./pnm2dat scaled/${i} scaled_dat/{i}.dat
+    done
+}
+
+mean() {
+    mean_time=$(awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }' $1)
+}
+
+std() {
+    std="$(awk '{sum+=$1; sumsq+=$1*$1} END {print sqrt(sumsq/NR - (sum/NR)**2)}' $1)"
+}
+
+run_cliph() {
+    for i in $(ls scaled_dat)
+    do
+        echo ${i}
+        ./$1 scaled_dat/${i}
+    done
+    awk 'NF>1{print $NF}' total_time.txt | sed 's/s$//' > times_$1
+    rm total_time.txt
+    rm -r results*
+}
+
+run_cliph pgi_cliphO1
+run_cliph pgi_cliphO2
+run_cliph pgi_cliphO3
+run_cliph pgi_cliph-none
+
+
+run_cliph gcc_cliphO1
+run_cliph gcc_cliphO2
+run_cliph gcc_cliphO3
+run_cliph gcc_cliph-none
