ddaa8eb20d9d8ce3c6c0ed835a07a88967a30c0a
Jacob Webber <s1116548@cplab153.ph.ed.ac.uk>
2016-11-30 19:53:37 +0000

    whats happenin

---
+++ message-passing-programming/coursework/mpi_solution/src/definitions.h
@@ -0,0 +1,16 @@
+
+#define M 768
+#define N 768
+
+#define MAXITER 1500
+#define PRINTFREQ 200
+
+#define P 4 
+
+#define MP M/2 
+#define NP N/2
+#define DIMX 2 
+#define DIMY 2
+
+typedef float real_number;
+#define MPI_REALNUMBER MPI_FLOAT
