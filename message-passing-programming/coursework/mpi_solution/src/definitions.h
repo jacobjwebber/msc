7bf8cae8ad481b19024a9d2c62a00cde3f50e9a2
Jacob Webber <s1116548@cplab153.ph.ed.ac.uk>
2016-12-03 09:34:08 +0000

    send to cirrus

--- message-passing-programming/coursework/mpi_solution/src/definitions.h
+++ message-passing-programming/coursework/mpi_solution/src/definitions.h
@@ -5,12 +5,12 @@
 //#define WIDTH 10
 //#define HEIGHT 10
 
-#define MAXITER 100
-#define PRINTFREQ 200
+#define MAXITER 10000
+#define PRINTFREQ 100000
 
 
-#define DIMX 3
-#define DIMY 3
+#define DIMX 4
+#define DIMY 4
 
 #define P DIMX*DIMY 
 #define WIDTH_P WIDTH / DIMX 
